d6c013bfef2076b1b110ffd2b79fa7a8aca6e219
zhenis-dastan <zdastan2007@gmail.com>
2026-04-27 12:46:46 +0500

    Create config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+# Database connection configuration
+DB_CONFIG = {
+    "host": "localhost",
+    "port": 5432,
+    "dbname": "phonebook",
+    "user": "postgres",
+    "password": "6676",  # Change to your actual password
+}
